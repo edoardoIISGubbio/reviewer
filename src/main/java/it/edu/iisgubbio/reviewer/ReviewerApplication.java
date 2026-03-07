2babdd1f09219f930ecff211b9bc9f26cd2921a4
Edoardo Panfili <edoardo.panfili@iisgubbio.edu.it>
2026-03-07 13:05:48 +0100

    aggiunta documentazione javadoc per sport e mobilità

--- src/main/java/it/edu/iisgubbio/reviewer/ReviewerApplication.java
+++ src/main/java/it/edu/iisgubbio/reviewer/ReviewerApplication.java
@@ -2,7 +2,10 @@ package it.edu.iisgubbio.reviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // @EnableAsync attiva un executor di default che gestisce autonomamente i thread
 // Spring avvia un thread quando arriva una chiamata a analyze()
@@ -14,6 +17,18 @@ public class ReviewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewerApplication.class, args);
+	}
+
+	@Bean
+	WebMvcConfigurer mvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addRedirectViewController("/mobilita", "/mobilita/it/edu/iisgubbio/oggetti/mobilita/package-summary.html");
+				registry.addRedirectViewController("/mobilità", "/mobilita/it/edu/iisgubbio/oggetti/mobilita/package-summary.html");
+				registry.addRedirectViewController("/sport",    "/sport/it/edu/iisgubbio/oggetti/sport/package-summary.html");
+			}
+		};
 	}
 
 }
---
+++ src/main/resources/static/sport/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"it.edu.iisgubbio.oggetti.sport","l":"Atleta"},{"p":"it.edu.iisgubbio.oggetti.sport","l":"Maratoneta"},{"p":"it.edu.iisgubbio.oggetti.sport","l":"Nuotatore"}];updateSearchResults();
